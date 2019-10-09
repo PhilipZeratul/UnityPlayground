2b2f4dec99b27eb0cf2a6a2ea1d48488a8357ed9
PhillpZhang <p.f.zeratul@gmail.com>
2019-10-09 23:25:41 +0800

    Shader_09_ComplexMaterials

---
+++ Assets/Shader_09_ComplexMaterial/Scritps/Editor/MyLightingShaderGUI.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+public class MyLightingShaderGUI : ShaderGUI
+{
+    private static GUIContent staticLabel = new GUIContent();
+    private static ColorPickerHDRConfig emissionConfig = new ColorPickerHDRConfig(0f, 99f, 1f / 99f, 3f);
+
+    private Material target;
+    private MaterialEditor materialEditor;
+    private MaterialProperty[] properties;
+
+    enum SmoothnessSource
+    {
+        Uniform, Albedo, Metallic
+    }
+
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        this.target = materialEditor.target as Material;
+        this.materialEditor = materialEditor;
+        this.properties = properties;
+
+        DoMain();
+        DoSecondary();
+        
+    }
+
+    private void DoMain()
+    {
+        GUILayout.Label("Main Maps", EditorStyles.boldLabel);
+        MaterialProperty mainTex = FindProperty("_MainTex");
+        materialEditor.TexturePropertySingleLine(MakeLabel(mainTex, "Albedo (RGB)"), mainTex, FindProperty("_Tint"));
+        DoNormals();
+        DoEmission();
+        DoMetallic();
+        DoSmoothness();
+        materialEditor.TextureScaleOffsetProperty(mainTex);
+    }
+
+    private void DoNormals()
+    {
+        MaterialProperty normalMap = FindProperty("_NormalMap");        
+        materialEditor.TexturePropertySingleLine(MakeLabel(normalMap), normalMap, normalMap.textureValue ? FindProperty("_BumpScale") : null);        
+    }
+
+    private void DoEmission()
+    {
+        MaterialProperty emissionMap = FindProperty("_EmissionMap");
+        EditorGUI.BeginChangeCheck();
+        materialEditor.TexturePropertyWithHDRColor(MakeLabel(emissionMap, "Emission (RGB)"), emissionMap, FindProperty("_Emission"), emissionConfig, false);
+        if (EditorGUI.EndChangeCheck())
+            SetKeyword("_EMISSION_MAP", emissionMap.textureValue);
+
+    }
+
+    private void DoMetallic()
+    {
+        MaterialProperty metallicMap = FindProperty("_MetallicMap");
+        EditorGUI.BeginChangeCheck();
+        materialEditor.TexturePropertySingleLine(MakeLabel(metallicMap, "Metallic (R)"), metallicMap, metallicMap.textureValue ? null : FindProperty("_Metallic"));
+        if (EditorGUI.EndChangeCheck())
+            SetKeyword("_METALLIC_MAP", metallicMap.textureValue);
+    }
+
+    private void DoSmoothness()
+    {
+        SmoothnessSource smoothnessSource = SmoothnessSource.Uniform;
+        if (IsKeywordEnabled("_SMOOTHNESS_ALBEDO"))
+            smoothnessSource = SmoothnessSource.Albedo;
+        else if (IsKeywordEnabled("_SMOOTHNESS_METALLIC"))
+            smoothnessSource = SmoothnessSource.Metallic;
+
+        EditorGUI.indentLevel += 2;
+        MaterialProperty slider = FindProperty("_Smoothness");
+        materialEditor.ShaderProperty(slider, MakeLabel(slider));
+        EditorGUI.indentLevel += 1;
+        EditorGUI.BeginChangeCheck();
+        RecordAction("Smoothness Source");
+        smoothnessSource = (SmoothnessSource)EditorGUILayout.EnumPopup(MakeLabel("Source"), smoothnessSource);
+        if (EditorGUI.EndChangeCheck())
+        {
+            SetKeyword("_SMOOTHNESS_ALBEDO", smoothnessSource == SmoothnessSource.Albedo);
+            SetKeyword("_SMOOTHNESS_METALLIC", smoothnessSource == SmoothnessSource.Metallic);
+        }
+            
+        EditorGUI.indentLevel -= 3;
+    }
+
+    private void DoSecondary()
+    {
+        GUILayout.Label("Secondary Maps", EditorStyles.boldLabel);
+        MaterialProperty detailTex = FindProperty("_DetailTex");
+        materialEditor.TexturePropertySingleLine(MakeLabel(detailTex, "Albedo (RGB) Multiplied by 2"), detailTex);
+        DoSecondaryNormals();
+        materialEditor.TextureScaleOffsetProperty(detailTex);
+    }
+
+    private void DoSecondaryNormals()
+    {
+        MaterialProperty detailNormalMap = FindProperty("_DetailNormalMap");
+        materialEditor.TexturePropertySingleLine(MakeLabel(detailNormalMap), detailNormalMap, detailNormalMap.textureValue ? FindProperty("_DetailBumpScale") : null);
+    }    
+
+    private MaterialProperty FindProperty(string name)
+    {
+        return FindProperty(name, properties);
+    }
+
+    private GUIContent MakeLabel(string name, string toolTip = null)
+    {
+        staticLabel.text = name;
+        staticLabel.tooltip = toolTip;
+        return staticLabel;
+    }
+
+    private GUIContent MakeLabel(MaterialProperty property, string toolTip = null)
+    {
+        return MakeLabel(property.displayName, toolTip);
+    }
+
+    private void SetKeyword(string keyword, bool state)
+    {
+        if (state)
+            target.EnableKeyword(keyword);
+        else
+            target.DisableKeyword(keyword);
+    }
+
+    private bool IsKeywordEnabled(string keyword)
+    {
+        return target.IsKeywordEnabled(keyword);
+    }
+
+    private void RecordAction(string label)
+    {
+        materialEditor.RegisterPropertyChangeUndo(label);
+    }
+}
---
+++ Assets/Shader_09_ComplexMaterial/Shaders 1/MyFirstLightingShader_ComplexMaterial_PBS.shader
@@ -0,0 +1,105 @@
+﻿Shader "Custom/My First Lighting Shader Complex Material PBS"
+{    
+    Properties
+    {
+        _Tint ("Tint", color) = (1, 1, 1, 1)
+        _MainTex ("Albedo", 2D) = "white" {}
+        //[NoScaleOffset] _HeightMap ("Height Map", 2D) = "gray" {}
+        [NoScaleOffset] _NormalMap ("Normal Map", 2D) = "bump" {}
+        _BumpScale ("Bump Scale", Float) = 1
+        [NoScaleOffset] _MetallicMap ("Metallic Map", 2D) = "white" {}
+        [Gamma] _Metallic ("Metallic", Range(0, 1)) = 1
+        _Smoothness ("Smoothness", Range(0, 1)) = 0.5
+        _DetailTex ("Detail Texture", 2D) = "gray" {}
+        [NoScaleOffset] _DetailNormalMap ("Detail Normal Map", 2D) = "bump" {}
+        _DetailBumpScale ("Detail Bump Scale", Float) = 1
+        [NoScaleOffset] _EmissionMap ("Emission Map", 2D) = "black" {}
+        _Emission ("Emission", Color) = (0, 0, 0)
+    }
+
+    CGINCLUDE
+
+    #define BINORMAL_PER_FRAGMENT
+
+    ENDCG
+
+	SubShader
+    {
+        Pass
+        {
+            Tags 
+            {
+                "LightMode" = "ForwardBase"
+            }
+
+            CGPROGRAM
+
+            #pragma target 3.0
+
+            #pragma multi_compile _ SHADOWS_SCREEN
+            #pragma multi_compile _ VERTEXLIGHT_ON
+            #pragma shader_feature _METALLIC_MAP
+            #pragma shader_feature _ _SMOOTHNESS_ALBEDO _SMOOTHNESS_METALLIC
+            #pragma shader_feature _EMISSION_MAP
+
+            #define FORWARD_BASE_PASS
+
+            #pragma vertex MyVertexProgram
+            #pragma fragment MyFragmentProgram
+
+            #include "MyLighting.cginc"
+            
+
+            ENDCG
+        }
+
+        Pass
+        {
+            Tags 
+            {
+                "LightMode" = "ForwardAdd"
+            }
+
+            Blend One One
+            ZWrite Off
+
+            CGPROGRAM
+
+            #pragma target 3.0
+
+            #pragma multi_compile_fwdadd_fullshadows
+            #pragma shader_feature _METALLIC_MAP
+            #pragma shader_feature _ _SMOOTHNESS_ALBEDO _SMOOTHNESS_METALLIC
+
+            #pragma vertex MyVertexProgram
+            #pragma fragment MyFragmentProgram           
+
+            #include "MyLighting.cginc"
+
+            ENDCG
+        }
+
+        Pass
+        {
+            Tags
+            {  
+                "LightMode" = "ShadowCaster"
+            }
+
+            CGPROGRAM
+
+            #pragma target 3.0
+
+            #pragma multi_compile_shadowcaster
+
+            #pragma vertex MyShadowVertexProgram
+            #pragma fragment MyShadowFragmentProgram
+
+            #include "MyShadows.cginc"
+
+            ENDCG
+        }
+    }
+
+    CustomEditor "MyLightingShaderGUI"
+}
